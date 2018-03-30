c265f654520d85df4b201c64aba04ee677b7943c
Evan Higgins <evanh98@uw.edu>
2018-03-30 13:56:44 -0700

    Successfully retreives necessary tags and data from pax site and pax west site. TODO: process and interpret the information

--- PAXNotifier/PAXNotifier.py
+++ PAXNotifier/PAXNotifier.py
@@ -1,14 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
+
+# website urls
 paxsite ='http://www.paxsite.com'
 westsite ='http://west.paxsite.com/'
-# fetch the content from url
+eastsite = 'http://east.paxsite.com/'
+southsite = 'http://south.paxsite.com/'
+aussite = 'http://aus.paxsite.com/'
+unpluggedsite = 'http://unplugged.paxsite.com/'
+devsite = 'http://dev.paxsite.com/'
+
+# fetch the content from urls
 paxsite_response = requests.get(paxsite, timeout=5)
 westsite_response = requests.get(westsite, timeout=5)
+eastsite_response = requests.get(eastsite, timeout=5)
+southsite_response = requests.get(southsite, timeout=5)
+aussite_response = requests.get(aussite, timeout=5)
+unpluggedsite_response = requests.get(unpluggedsite, timeout=5)
+devsite_response = requests.get(devsite, timeout=5)
+
 # parse html
 paxsite_content = BeautifulSoup(paxsite_response.content, "html.parser")
-paxsite_west_status = paxsite_content.find(id='west').find('p').getText().lower()
 westsite_content = BeautifulSoup(westsite_response.content, "html.parser")
-# westsite_status = westsite_content.find()
+eastsite_content = BeautifulSoup(eastsite_response.content, "html.parser")
+southsite_content = BeautifulSoup(southsite_response.content, "html.parser")
+aussite_content = BeautifulSoup(aussite_response.content, "html.parser")
+unpluggedsite_content = BeautifulSoup(unpluggedsite_response.content, "html.parser")
+devsite_content = BeautifulSoup(devsite_response.content, "html.parser")
 
-print(paxsite_west_status=='see you next time')
+paxsite_west_status = paxsite_content.find(id='west').find('p').getText().lower()
+westsite_hero = westsite_content.findAll('section','slide-text')
+westiste_hero_headers = []
+westsite_hero_links = []
+for sec in westsite_hero:
+    westiste_hero_headers.append(sec.find('h1').getText())
+    westsite_hero_links.append(sec.find('a').getText())
+
+westsite_message = westsite_content.find('div','message')
+westsite_reg = westsite_content.find(id='reg')
