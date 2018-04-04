db591045093402bf7216b3d83ecf5ed5bf9ba507
Evan Higgins <evanh98@uw.edu>
2018-04-04 14:37:14 -0700

    Retrieved PAX West Facebook page. ToDo extract posts

--- PAXNotifier/PAXNotifier.py
+++ PAXNotifier/PAXNotifier.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
+import facebook
+import urllib3
+
+# Web Scraping
 
 # website urls
 paxsite ='http://www.paxsite.com'
@@ -20,13 +24,13 @@ unpluggedsite_response = requests.get(unpluggedsite, timeout=5)
 devsite_response = requests.get(devsite, timeout=5)
 
 # parse html
-paxsite_content = BeautifulSoup(paxsite_response.content, "html.parser")
-westsite_content = BeautifulSoup(westsite_response.content, "html.parser")
-eastsite_content = BeautifulSoup(eastsite_response.content, "html.parser")
-southsite_content = BeautifulSoup(southsite_response.content, "html.parser")
-aussite_content = BeautifulSoup(aussite_response.content, "html.parser")
-unpluggedsite_content = BeautifulSoup(unpluggedsite_response.content, "html.parser")
-devsite_content = BeautifulSoup(devsite_response.content, "html.parser")
+paxsite_content = BeautifulSoup(paxsite_response.content, 'html.parser')
+westsite_content = BeautifulSoup(westsite_response.content, 'html.parser')
+eastsite_content = BeautifulSoup(eastsite_response.content, 'html.parser')
+southsite_content = BeautifulSoup(southsite_response.content, 'html.parser')
+aussite_content = BeautifulSoup(aussite_response.content, 'html.parser')
+unpluggedsite_content = BeautifulSoup(unpluggedsite_response.content, 'html.parser')
+devsite_content = BeautifulSoup(devsite_response.content, 'html.parser')
 
 paxsite_west_status = paxsite_content.find(id='west').find('p').getText().lower()
 westsite_hero = westsite_content.findAll('section','slide-text')
@@ -38,3 +42,11 @@ for sec in westsite_hero:
 
 westsite_message = westsite_content.find('div','message')
 westsite_reg = westsite_content.find(id='reg')
+
+#Facebook
+
+f = open('Token.txt','r')
+token = f.read()
+graph = facebook.GraphAPI(access_token=token, version = '2.12')
+
+pages = graph.search(type='user', q='PAX West')
